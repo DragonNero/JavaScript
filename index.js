f8e2156cca09217870cc7277f120a1b22b06812a
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-13 17:00:45 +0200

    added slice function for my tweet characters counter

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var tweet = prompt("Your tweet");
 tweet.length;
 var entered = tweet.length;
-var remained = 280 - tweet.length;
-alert("You entered " + tweet.length + " characters, " + remained + " characters remained." );
+var remained = 140 - tweet.length;
+if (tweet.length > 140) {
+  alert ("Your tweet is too big, we cutted first 140 characters: " + (tweet.slice(0, 139)));
+}
+else {
+  alert ("Your tweet's length is ok");
+}
