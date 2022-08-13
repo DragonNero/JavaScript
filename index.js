55603c9f0fc790f3f109a2d929c364bf258f38f5
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-13 18:48:03 +0200

    solved upper lower case name challenge

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var tweet = prompt("Your tweet");
-tweet.length;
-var entered = tweet.length;
-var remained = 140 - tweet.length;
-if (tweet.length > 140) {
-  alert ("Your tweet is too big, we cutted first 140 characters: " + (tweet.slice(0, 139)));
-}
-else {
-  alert ("Your tweet's length is ok");
-}
+var name = prompt("What is your name?");
+var newName = name.toUpperCase();
+var firstLetter = newName.slice(0,1);
+var restOfTheName = name.slice(1,name.length);
+restOfTheName = restOfTheName.toLowerCase();
+alert("Hello "+ firstLetter+restOfTheName+" !")
+
+
+
+
+
+
+// var entered = tweet.length;
+// var remained = 140 - tweet.length;
+// if (tweet.length > 140) {
+//   alert ("Your tweet is too big, we cutted first 140 characters: " + (tweet.slice(0, 139)));
+// }
+// else {
+//   alert ("Your tweet's length is ok");
+// }
