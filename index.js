382fa2a9e33876f6c074817a07ff71e14ec0f842
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-14 07:40:54 +0200

    added human dog age converter

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-var name = prompt("What is your name?");
-var newName = name.toUpperCase();
-var firstLetter = newName.slice(0,1);
-var restOfTheName = name.slice(1,name.length);
-restOfTheName = restOfTheName.toLowerCase();
-alert("Hello "+ firstLetter+restOfTheName+" !")
+// var name = prompt("What is your name?");
+// var newName = name.toUpperCase();
+// var firstLetter = newName.slice(0,1);
+// var restOfTheName = name.slice(1,name.length);
+// restOfTheName = restOfTheName.toLowerCase();
+// alert("Hello "+ firstLetter+restOfTheName+" !")
+//
 
+// humanAge = (dogAge - 2)*4 + 21
+function isPositiveInteger(str) {
+  if (typeof str !== 'string') {
+    return false;
+  }
 
+  const num = Number(str);
+
+  if (Number.isInteger(num) && num > 0) {
+    return true;
+  }
+
+  return false;
+}
+
+var yourDogAge = prompt("What is the age of your dog?");
+if (!isPositiveInteger(yourDogAge)) {
+  alert("It must be a number");
+}
+var humanAgeOfTheDog = (yourDogAge -2)*4+21;
+alert("The human age of your dog is " + humanAgeOfTheDog +" years.");
 
 
 
