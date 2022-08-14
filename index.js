1ac4096c359bae75f106785a89c1a71b094eca8d
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-14 08:42:40 +0200

    Created a life time left calculator

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-function getMilk(money) {
-  console.log("leaveHouse");
-  console.log("moveRight");
-  console.log("moveRight");
-  console.log("moveUp");
-  console.log("moveUp");
-  console.log("moveUp");
-  console.log("moveUp");
-  console.log("moveRight");
-  console.log("moveRight");
-  var buyMilk = Math.floor(money/=1.5);
-  console.log("I will buy with this money "+ buyMilk +" bottles of milk");
-  console.log("moveLeft");
-  console.log("moveLeft");
-  console.log("moveDown");
-  console.log("moveDown");
-  console.log("moveDown");
-  console.log("moveDown");
-  console.log("moveLeft");
-  console.log("moveLeft");
-  console.log("enterHouse");
+// function getMilk(money) {
+//   console.log("leaveHouse");
+//   console.log("moveRight");
+//   console.log("moveRight");
+//   console.log("moveUp");
+//   console.log("moveUp");
+//   console.log("moveUp");
+//   console.log("moveUp");
+//   console.log("moveRight");
+//   console.log("moveRight");
+//   var buyMilk = Math.floor(money/=1.5);
+//   console.log("I will buy with this money "+ buyMilk +" bottles of milk");
+//   console.log("moveLeft");
+//   console.log("moveLeft");
+//   console.log("moveDown");
+//   console.log("moveDown");
+//   console.log("moveDown");
+//   console.log("moveDown");
+//   console.log("moveLeft");
+//   console.log("moveLeft");
+//   console.log("enterHouse");
+// }
+// getMilk(5);
+
+
+function lifeInWeeks(age) {
+  var lifeInYears = (90 - age);
+  var lifeInDays = (lifeInYears * 365);
+  var lifeInWeeks = (lifeInYears * 52);
+  var lifeInMonths = (lifeInYears/12);
+  console.log("You have" + lifeInDays + " days, " +  lifeInWeeks + "weeks, and " + lifeInMonths +" months left. ");
 }
-getMilk(5);
+
+lifeInWeeks(31)
+// Example Input
+//
+// e.g. If you are 56 years old:
+//
+// lifeInWeeks(56)
+// Example Output
+//
+//
+// Hint
