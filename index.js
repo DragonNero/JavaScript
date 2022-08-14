6322e60953fb562b4268639d04655dcddc2c28b7
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-14 08:15:11 +0200

    more complex function with var inside

--- index.js
+++ index.js
@@ -1,41 +1,23 @@
-// var name = prompt("What is your name?");
-// var newName = name.toUpperCase();
-// var firstLetter = newName.slice(0,1);
-// var restOfTheName = name.slice(1,name.length);
-// restOfTheName = restOfTheName.toLowerCase();
-// alert("Hello "+ firstLetter+restOfTheName+" !")
-//
-
-// humanAge = (dogAge - 2)*4 + 21
-function isPositiveInteger(str) {
-  if (typeof str !== 'string') {
-    return false;
-  }
-
-  const num = Number(str);
-
-  if (Number.isInteger(num) && num > 0) {
-    return true;
-  }
-
-  return false;
+function getMilk(money) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  var buyMilk = Math.floor(money/=1.5);
+  console.log("I will buy with this money "+ buyMilk +" bottles of milk");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
 }
-
-var yourDogAge = prompt("What is the age of your dog?");
-if (!isPositiveInteger(yourDogAge)) {
-  alert("It must be a number");
-}
-var humanAgeOfTheDog = (yourDogAge -2)*4+21;
-alert("The human age of your dog is " + humanAgeOfTheDog +" years.");
-
-
-
-
-// var entered = tweet.length;
-// var remained = 140 - tweet.length;
-// if (tweet.length > 140) {
-//   alert ("Your tweet is too big, we cutted first 140 characters: " + (tweet.slice(0, 139)));
-// }
-// else {
-//   alert ("Your tweet's length is ok");
-// }
+getMilk(5);
