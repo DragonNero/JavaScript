7d48ff6b6c682475654de0bf18f5f2a9d69951d0
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-14 10:58:55 +0200

    robot getMilk function

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-// function getMilk(money) {
-//   console.log("leaveHouse");
-//   console.log("moveRight");
-//   console.log("moveRight");
-//   console.log("moveUp");
-//   console.log("moveUp");
-//   console.log("moveUp");
-//   console.log("moveUp");
-//   console.log("moveRight");
-//   console.log("moveRight");
-//   var buyMilk = Math.floor(money/=1.5);
-//   console.log("I will buy with this money "+ buyMilk +" bottles of milk");
-//   console.log("moveLeft");
-//   console.log("moveLeft");
-//   console.log("moveDown");
-//   console.log("moveDown");
-//   console.log("moveDown");
-//   console.log("moveDown");
-//   console.log("moveLeft");
-//   console.log("moveLeft");
-//   console.log("enterHouse");
-// }
-// getMilk(5);
-
-
-function lifeInWeeks(age) {
-  var lifeInYears = (90 - age);
-  var lifeInDays = (lifeInYears * 365);
-  var lifeInWeeks = (lifeInYears * 52);
-  var lifeInMonths = (lifeInYears/12);
-  console.log("You have" + lifeInDays + " days, " +  lifeInWeeks + "weeks, and " + lifeInMonths +" months left. ");
+function getMilk(money) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  var buyMilk = Math.floor(money/=1.5);
+  console.log("I will buy with this money "+ buyMilk +" bottles of milk");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
 }
+getMilk(5);
+
+
+// function lifeInWeeks(age) {
+//   var lifeInYears = (90 - age);
+//   var lifeInDays = (lifeInYears * 365);
+//   var lifeInWeeks = (lifeInYears * 52);
+//   var lifeInMonths = (lifeInYears/12);
+//   console.log("You have" + lifeInDays + " days, " +  lifeInWeeks + "weeks, and " + lifeInMonths +" months left. ");
+// }
 
 lifeInWeeks(31)
 // Example Input
