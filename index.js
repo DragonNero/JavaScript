7313df91c2a28e19c1e9b26f21cd02cd61bcd37d
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-15 23:04:39 +0200

    added bmi calculator

--- index.js
+++ index.js
@@ -68,3 +68,19 @@ if (yourMatch < 80 && yourMatch >= 30 ) {
 if (yourMatch < 30 ) {
   alert("Your match is " + Math.round(yourMatch) + "%. It's Ok. ");
   };
+
+
+
+  function bmiCalculator (weight, height) {
+  var BMI = Math.round(weight / (height*height) );
+  if (BMI <18.5) {
+  return "Your BMI is "+ BMI + ", so you are underweight."}
+
+if (BMI > 18.5 && BMI < 24.9) {
+  return "Your BMI is " + BMI + ", so you have a normal weight."}
+
+if (BMI > 24.9) {
+  return "Your BMI is " + BMI + ", so you are overweight."
+} }
+  
+ bmiCalculator(65,1.6);
