38e14e9b9a53a1d6d89ed7fa69371546651f322a
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-21 23:41:25 +0200

    finally founded good solution for fizz buzz problem

--- index.js
+++ index.js
@@ -117,6 +117,7 @@ else {
 };
 
 //
+
 // fizzBuzz solution
 var output = [];
 
@@ -124,3 +125,26 @@ function fizzBuzz() {
     output.push(output.length + 1);
     console.log(output);
 }
+fizzBuzz();
+
+// fizzBuzz solution 2
+let output = [];
+let count = 1;
+
+function fizzBuzz() {
+  while(count<= 100) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0 ) {
+      output.push("Fizz");
+    } else if (count % 5 === 0 ) {
+      output.push("Buzz");
+    } else {
+      output.push(count);
+    }
+    count++;
+  }
+  console.log(output);
+}
+
+fizzBuzz();
